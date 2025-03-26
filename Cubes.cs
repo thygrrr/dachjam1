3a411460244bd85f46c52da041ea54d7031ac5cb
Tiger Jove <tiger@tiger.blue>
2025-03-26 22:20:20 +0100

    ECS instance parameters functional.

--- Cubes.cs
+++ Cubes.cs
@@ -9,8 +9,8 @@ namespace jam;
 
 public partial class Cubes : MultiMeshInstance3D
 {
-    private static World World => ECS.World;
-    private Stream<Matrix4X3, Index, Tile, Cubes> _cubes;
+    private static World World => code.ECS.World;
+    private Stream<Matrix4X3Custom, Index, Tile, Cubes> _cubes;
 
     private List<Tile> _tiles = [];
     
@@ -19,10 +19,10 @@ public partial class Cubes : MultiMeshInstance3D
     
     private FastNoiseLite _noiseLite = new()
     {
-        Frequency = 0.025f,
+        Frequency = 0.125f,
     };
     
-    private const int Count = 1000;
+    private const int Count = 10000;
     
     private readonly Random _random = new();
     public override void _Ready()
@@ -30,23 +30,27 @@ public partial class Cubes : MultiMeshInstance3D
         base._Ready();
         Multimesh.InstanceCount = Count;
         Multimesh.VisibleInstanceCount = Count;
-        _cubes = World.Query<Matrix4X3, Index, Tile, Cubes>(default, default, default, Link.With(this)).Has<Cube>().Stream();
+        _cubes = World.Query<Matrix4X3Custom, Index, Tile, Cubes>(default, default, default, Link.With(this)).Has<Cube>().Stream();
 
         using var spawner = World.Entity()
             .Add<Cube>()
-            .Add<Matrix4X3>()
+            .Add<Matrix4X3Custom>()
             .Add<Index>()
             .Add<Tile>()
             .Add(Link.With(this))
             .Spawn(Count);
         
         var i = 0;
-        _cubes.For((ref Matrix4X3 matrix, ref Index index, ref Tile tile) =>
+        _cubes.For((ref Matrix4X3Custom matrix, ref Index index, ref Tile tile) =>
         {
-            matrix = new(new(_random.NextSingle() * 5, 0, _random.NextSingle() * 5), 1);
+            matrix = new(new(_random.NextSingle() * 5, 0, _random.NextSingle() * 5))
+            {
+                Custom0 = Random.Shared.NextSingle(),
+            };
+
             index = new(i++);
-            var j = i/20;
-            tile = new(j%20, j/20);
+            var j = i/50;
+            tile = new(j%10, j/30);
         });
     }
     
@@ -57,24 +61,30 @@ public partial class Cubes : MultiMeshInstance3D
         base._Process(delta);
         _timeAccumulator += dt;
         
-        _cubes.For((_timeAccumulator, _noiseLite), ((float time, FastNoiseLite noise) uniform, ref Matrix4X3 matrix, ref Index index, ref Tile tile) =>
+        _cubes.Job(
+            uniform: (_timeAccumulator, _noiseLite), 
+            action: ((float time, FastNoiseLite noise) uniform, ref Matrix4X3Custom matrix, ref Index index, ref Tile tile) =>
         {
-            float iv = (float) index.Value * 20f;
-            float jv = (float) index.Value * 30f;
+            var iv = index.Value * 20f;
+            var jv = index.Value * 30f;
+            
             var brownianX = uniform.noise.GetNoise3D(tile.X, uniform.time, tile.Z + jv); 
+            var brownianY = uniform.noise.GetNoise3D(tile.X + iv, tile.Z, uniform.time) * 0.1f; 
             var brownianZ = uniform.noise.GetNoise3D(tile.X, tile.Z + iv, uniform.time); 
-            var pos = new Vector3(tile.X , 0, tile.Z) + new Vector3(brownianX, 0, brownianZ);
-            matrix = new(pos);
+            var pos = new Vector3(tile.X , brownianY, tile.Z) + new Vector3(brownianX, 0, brownianZ);
+            matrix = new(pos)
+            {
+                Custom0 = Mathf.Sin(uniform.time + index.Value) + 1.0f,
+            };
         });
         
         _cubes.Raw(
-            uniform: (Multimesh.GetRid(), Count * Matrix4X3.SizeInFloats),
-            action:  static ((Rid mesh, int count) uniform, Memory<Matrix4X3> transforms) =>
+            uniform: (Multimesh.GetRid(), Count * Matrix4X3Custom.SizeInFloats),
+            action: static ((Rid mesh, int count) uniform, Memory<Matrix4X3Custom> transforms) =>
             {
-                var floatSpan = MemoryMarshal.Cast<Matrix4X3, float>(transforms.Span);
+                var floatSpan = MemoryMarshal.Cast<Matrix4X3Custom, float>(transforms.Span);
                 RenderingServer.MultimeshSetBuffer(uniform.mesh, floatSpan[..uniform.count]);
             });
-
     }
     
     private record struct Cube;
@@ -83,6 +93,7 @@ public partial class Cubes : MultiMeshInstance3D
     
     private record struct Tile(int X, int Z)
     {
+        
     }
 }
 
--- code/ECS.cs
+++ code/ECS.cs
@@ -1,5 +1,8 @@
-using Godot;
 using fennecs;
+using Godot;
+
+namespace jam.code;
+
 public partial class ECS : Node
 {
     public static readonly World World = new();
